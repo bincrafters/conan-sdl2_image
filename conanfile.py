5da215f1a25ace28f6d17a904686fb295a439ff2
SSE4 <tomskside@gmail.com>
2018-05-05 19:39:30 +0700

    - some very naive prototype for Windows (doesn't work)
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, MSBuild, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
+class SDL2ImageConan(ConanFile):
+    name = "sdl2_image"
+    version = "2.0.3"
+    description = "SDL_image is an image file loading library"
+    url = "https://github.com/bincrafters/conan-sdl2_image"
+    homepage = "https://www.libsdl.org/projects/SDL_image/"
 
     # Indicates License type of the packaged library
     license = "MIT"
 
     # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
@@ -31,14 +27,8 @@ class LibnameConan(ConanFile):
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
 
-    # Use version ranges for dependencies unless there's a reason not to
-    # Update 2/9/18 - Per conan team, ranges are slow to resolve.
-    # So, with libs like zlib, updates are very rare, so we now use static version
-
-
     requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/1.2.11@conan/stable"
+        "sdl2/2.0.8@bincrafters/stable"
     )
 
     def config_options(self):
@@ -46,29 +36,29 @@ class LibnameConan(ConanFile):
             del self.options.fPIC
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
-
-        #Rename to "source_subfolder" is a convention to simplify later steps
+        source_url = "https://www.libsdl.org/projects/SDL_image/release/SDL2_image-%s.tar.gz" % self.version
+        tools.get(source_url)
+        extracted_dir = "SDL2_image-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
-    def configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-        cmake.configure(build_folder=self.build_subfolder)
-        return cmake
-
     def build(self):
-        cmake = self.configure_cmake()
-        cmake.build()
+        if self.settings.compiler == 'Visual Studio':
+            self.build_msvc()
+        else:
+            self.build_configure()
+
+    def build_msvc(self):
+        with tools.chdir(os.path.join(self.source_subfolder, 'VisualC')):
+            msbuild = MSBuild(self)
+            msbuild.build_env.link_flags = ['/lib', 'winmm.lib']
+            msbuild.build('SDL_image.sln')
+
+    def build_configure(self):
+        raise Exception('TODO')
 
     def package(self):
+        raise Exception('TODO')
         self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
-        cmake.install()
         # If the CMakeLists.txt has a proper install method, the steps below may be redundant
         # If so, you can just remove the lines below
         include_folder = os.path.join(self.source_subfolder, "include")
